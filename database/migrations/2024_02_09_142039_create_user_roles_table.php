621d7bc2ea450faa9116d152fb15efa13f10052d
poonambharara87 <poonambharara87@gmail.com>
2024-02-09 19:58:48 +0530

    migrate

--- database/migrations/2024_02_09_142039_create_user_roles_table.php
+++ database/migrations/2024_02_09_142039_create_user_roles_table.php
@@ -11,15 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->bigInteger('role_id')->unsigned()->index()->nullable();
-            $table->foreign('role_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -29,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_roles');
     }
 };
